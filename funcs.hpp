01ddebb5a7c313ee7cdf67d435935adcc2e61a1d
Pranav Tripathi <tripathi.pranav@outlook.com>
2020-10-22 11:25:33 -0400

    created framwork for player turn

--- funcs.hpp
+++ funcs.hpp
@@ -23,3 +23,54 @@ void draw_board(int a[3][3])
     }
     cout << '\n';
 }
+
+void player_turn(int a[3][3])
+{
+    int temp_var = 0;
+    cout << "Which square? ";
+    cin >> temp_var;
+
+    switch(temp_var){
+        case 1:
+            a[0][0] += 1;
+            break;
+
+        case 2:
+            a[0][1] += 1;
+            break;
+
+        case 3:
+            a[0][2] += 1;
+            break;
+
+        case 4:
+            a[1][0] += 1;
+            break;
+
+        case 5:
+            a[1][1] += 1;
+            break;
+
+        case 6:
+            a[1][2] += 1;
+            break;
+        
+        case 7:
+            a[2][0] += 1;
+            break;
+
+        case 8:
+            a[2][1] += 1;
+            break;
+
+        case 9:
+            a[2][2] += 1;
+            break;
+        
+        default:
+            cout << "invalid entry!";
+            player_turn(a);
+            break;
+    }
+    //TODO: ADD CHECK FOR SQUARE BEING SELECTED ALREADY
+}
--- main.cpp
+++ main.cpp
@@ -9,4 +9,6 @@ int main()
                            {empty, empty, empty},
                            {empty, empty, empty}};
     draw_board(gameboard);
+    player_turn(gameboard);
+    draw_board(gameboard);
 }
